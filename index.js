dcc796bd05d25d6afed53d1e8a1c8ed0c6f7f50a
Jay McKerracher <jaymckerracher@gmail.com>
2026-02-24 15:44:20 +0000

    add functions to create oscillators

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 const audioCtx = new AudioContext();
 const button = document.querySelector("button");
 
-// create and connect the oscillator
-function createOscillator() {
+// function to create oscillator
+function createOscillator(freq) {
     const oscillator = audioCtx.createOscillator();
 
-    oscillator.frequency.setValueAtTime(220, audioCtx.currentTime);
+    oscillator.frequency.setValueAtTime(freq, audioCtx.currentTime);
 
     oscillator.connect(audioCtx.destination);
 
     return oscillator;
 };
 
-let oscillator;
+// function to create a list of oscillators
+function createManyOsciallators(startingFreq, quantity) {
+    const oscillators = [];
+    let freq = startingFreq;
 
-// start stop osciallator via button
-button.addEventListener("click", () => {
-    if (audioCtx.state === "suspended") {
-        audioCtx.resume();
+    for (let i=0; i<quantity; i++) {
+        oscillators.push(createOscillator(freq));
+        freq *= 2;
     }
 
-    if (button.dataset.playing == "false") {
-        oscillator = createOscillator();
-        oscillator.start();
-        button.dataset.playing = "true";
-    } else {
-        oscillator.stop();
-        button.dataset.playing = "false";
-    };
-});
+    return oscillators;
+}
+
+// creating the oscillators
+const oscillators = createManyOsciallators(110, 7);
+console.log(oscillators);
