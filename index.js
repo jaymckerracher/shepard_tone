020041ba4a2e75e35eb764425d28190252553ab1
Jay McKerracher <jaymckerracher@gmail.com>
2026-02-24 15:38:41 +0000

    create basic sine wave generator

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("Hello World!");
+const audioCtx = new AudioContext();
+const button = document.querySelector("button");
+
+// create and connect the oscillator
+function createOscillator() {
+    const oscillator = audioCtx.createOscillator();
+
+    oscillator.frequency.setValueAtTime(220, audioCtx.currentTime);
+
+    oscillator.connect(audioCtx.destination);
+
+    return oscillator;
+};
+
+let oscillator;
+
+// start stop osciallator via button
+button.addEventListener("click", () => {
+    if (audioCtx.state === "suspended") {
+        audioCtx.resume();
+    }
+
+    if (button.dataset.playing == "false") {
+        oscillator = createOscillator();
+        oscillator.start();
+        button.dataset.playing = "true";
+    } else {
+        oscillator.stop();
+        button.dataset.playing = "false";
+    };
+});
